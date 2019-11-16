af8d56f67fdf7f5d0afdee5502c02e19cd17f1bc
niya1123 <yoheikurioka@gmail.com>
2019-11-16 10:28:40 +0900

    とりあえずgetとpostができるようになった

---
+++ java_dir/Qtroid/src/main/java/test/TestServlet.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        String name = (String) req.getAttribute("userName");
+
+        if(name == null || "".equals(name))
+            req.setAttribute("userName", "Guest");
+        
+        String view_path = "/WEB-INF/views/index.jsp";
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher(view_path);
+
+        dispatcher.forward(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        
+        req.setCharacterEncoding("utf-8");
+
+        String name = req.getParameter("name");
+
+        req.setAttribute("userName", name);
+
+        doGet(req, resp);
+    }
+}
---
+++ java_dir/Qtroid/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html ; characterset=UTF-8" pageEncoding="UTF-8" %>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>To-Do-List</title>
+    </head>
+    <body>
+        <% String userName = (String)request.getAttribute("userName"); %>
+        こんにちは、<%= userName %>さん!
+
+        <% if("Guest".equals(userName)) { %>
+        <form method="POST" action="">
+            何か入力してください: <input type="text" name="name">
+            <button tyoe="submit">送信</button>
+        </form>
+        <% } %>
+    </body>
+</html>
