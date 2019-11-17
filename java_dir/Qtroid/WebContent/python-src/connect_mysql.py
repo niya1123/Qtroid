b2e4fefcf642b0dad69056321da70f924808cc99
niya1123 <yoheikurioka@gmail.com>
2019-11-17 15:18:43 +0900

    タグランキングのmysqlの登録の実装完了

--- java_dir/Qtroid/WebContent/python-src/connect_mysql.py
+++ java_dir/Qtroid/WebContent/python-src/connect_mysql.py
@@ -1,14 +1,47 @@
 # mysqlへの接続お試し
 
+# import mysql.connector
+
+# conn = mysql.connector.connect(user='root', password='root', host='mysql', database='test')
+# cur = conn.cursor()
+
+# cur.execute("select * from user;")
+
+# for row in cur.fetchall():
+#     print(row[0],row[1])
+
+# cur.close
+# conn.close
+
 import mysql.connector
 
-conn = mysql.connector.connect(user='root', password='root', host='mysql', database='test')
-cur = conn.cursor()
+class ConnectMySQL():
+    """
+    MySQLに接続して, 操作するクラス.
+    """
 
-cur.execute("select * from user;")
+    def __init__(self):
+        """
+        MySQLに接続するために必要ものの初期設定.
+        """
+        self.conn = mysql.connector.connect(
+            user='user', 
+            password='userpass', 
+            host='mysql', 
+            database='qiita_rank',
+            charset='utf8')
+        self.cur = self.conn.cursor()
 
-for row in cur.fetchall():
-    print(row[0],row[1])
+    def register_tag_ranking(self, tag_ranking_data: dict):
+        """
+        MySQLのtag_rankingテーブルに値を入れる為の関数.
+        """
+        self.cur.execute("DROP TABLE IF EXISTS tag_ranking")
 
-cur.close
-conn.close
+        self.cur.execute("CREATE TABLE tag_ranking (ranking int NOT NULL, tag_name VARCHAR(50) NOT NULL)")
+
+        for rank in list(tag_ranking_data.keys()):
+            self.cur.execute("INSERT INTO tag_ranking (ranking, tag_name) VALUES (%d, '%s')"%(rank, tag_ranking_data[rank].decode('utf-8')))
+
+        self.conn.commit()
+        self.conn.close()
--- java_dir/Qtroid/WebContent/python-src/scrape_qiita.py
+++ java_dir/Qtroid/WebContent/python-src/scrape_qiita.py
@@ -3,9 +3,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
-from lxml.html import fromstring, tostring
-import time
-import os
+import connect_mysql
 
 class QiitaGetRanking():
     """
@@ -31,15 +29,15 @@ class QiitaGetRanking():
 
         Returns
         -------
-        ranking_data: dict
+        tag_ranking_data: dict
             タグランキングを収めた辞書オブジェクト.
         """
         soup = BeautifulSoup(self.html, "html.parser")
         ra_tag_names = soup.find_all(class_='ra-Tag_name pr-1')
-        ranking_data = {}
+        tag_ranking_data = {}
         for i, ra_tag_name in enumerate(ra_tag_names):
-            ranking_data[i+1] = ra_tag_name.text.encode(self.encoding)
-        return ranking_data
+            tag_ranking_data[i+1] = ra_tag_name.text.encode(self.encoding)
+        return tag_ranking_data
 
 if __name__ == "__main__":
     """
@@ -53,7 +51,9 @@ if __name__ == "__main__":
         qgr = QiitaGetRanking(browser)
         browser.close()
         browser.quit()
-        qgr.get_tag_ranking()
+        cm = connect_mysql.ConnectMySQL()
+        cm.register_tag_ranking(qgr.get_tag_ranking())
+
     except:
         browser.close()
         browser.quit()
