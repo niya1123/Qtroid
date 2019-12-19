175f568b4d62e84b5a43ad0eb73258a0e4015058
niya1123 <yoheikurioka@gmail.com>
2019-12-19 14:53:54 +0900

    update api

--- java_dir/Qtroid/WebContent/python-src/api.py
+++ java_dir/Qtroid/WebContent/python-src/api.py
@@ -1,17 +1,35 @@
-from flask import Flask
-
-import subprocess
+from flask import Flask, jsonify
+import qtroid
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False #日本語文字化け対策
+app.config["JSON_SORT_KEYS"] = False #ソートをそのまま
+
+
+def get_qtroid():
+    return qtroid.QiitaGetRanking()
 
 @app.route('/')
 def index():
     return "Hello world!!"
 
-@app.route('/api/registerMySQL')
-def api_registermySQL():
-    subprocess.check_call(['python','qtroid.py'])
-    return "Hello api world!!"
+@app.route('/api/get_tag_ranking')
+def api_get_tag_ranking():
+    qgr = get_qtroid()
+    tag_ranking_data = qgr.get_tag_ranking()
+    qgr.close_browser()
+    return jsonify({
+            'tag_ranking_data':tag_ranking_data
+        })
+
+@app.route('/api/get_tag_trend')
+def api_get_tag_trend():
+    qgr = get_qtroid()
+    tag_trend_data = qgr.get_tag_ranking()
+    qgr.close_browser()
+    return jsonify({
+            'tag_trend_data':tag_ranking_data
+        })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- java_dir/Qtroid/WebContent/python-src/qtroid.py
+++ java_dir/Qtroid/WebContent/python-src/qtroid.py
@@ -10,7 +10,17 @@ class QiitaGetRanking():
     Qiitaからランキングエータを取得し, mysqlに登録するクラス.
     """
 
-    def get_tag_ranking(self, browser: webdriver) -> dict:
+    def __init__(self):
+        self.browser = webdriver.Remote(
+        command_executor='http://selenium-hub:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME)
+    
+    def close_browser(self):
+        self.browser.close()
+        self.browser.quit()
+
+    def get_tag_ranking(self) -> dict:
+    # def get_tag_ranking(self, browser:webdriver) -> dict:
         """
         Qiitaからタグランキングに関する情報を取得する関数.
 
@@ -19,6 +29,9 @@ class QiitaGetRanking():
         tag_ranking_data: dict
             タグランキングを収めた辞書オブジェクト.
         """
+        browser = self.browser
+        browser.get('https://qiita.com')
+        WebDriverWait(browser, 15).until(EC.presence_of_all_elements_located)
         html = browser.page_source.encode('utf-8')
         soup = BeautifulSoup(html, "html.parser")
         ra_tag_names = soup.find_all(class_='ra-Tag_name pr-1')[:5]
@@ -28,7 +41,9 @@ class QiitaGetRanking():
             'https://qiita.com/tags/%s'%(ra_tag_name.text.lower())]
         return tag_ranking_data
     
-    def get_trend_data(self, tag_ranking_data: dict, browser: webdriver):
+    def get_trend_data(self, tag_ranking_data: dict):
+    # def get_trend_data(self, tag_ranking_data: dict, browser:webdriver):
+        browser = self.browser
         trend_data = {}
 
         for ranking in list(tag_ranking_data.keys()):
@@ -52,18 +67,20 @@ if __name__ == "__main__":
     main文. browserはhtmlの取得が終わり次第閉じること.エラーが出てきたときも同様.
     """
     
-    browser = webdriver.Remote(
-        command_executor='http://selenium-hub:4444/wd/hub',
-        desired_capabilities=DesiredCapabilities.CHROME)
-    print("start scrape")
-    browser.get('https://qiita.com')
-    WebDriverWait(browser, 15).until(EC.presence_of_all_elements_located)
+    # print("create browser")
+    # browser = webdriver.Remote(
+    #     command_executor='http://selenium-hub:4444/wd/hub',
+    #     desired_capabilities=DesiredCapabilities.CHROME)
     print("generate object")
     qgr = QiitaGetRanking()
-    ranking_data = qgr.get_tag_ranking(browser)
-    trend_data = qgr.get_trend_data(ranking_data, browser)
-    browser.close()
-    browser.quit()
+    print("start scrape")
+    # ranking_data = qgr.get_tag_ranking(browser)
+    # trend_data = qgr.get_trend_data(ranking_data, browser)
+    ranking_data = qgr.get_tag_ranking()
+    trend_data = qgr.get_trend_data(ranking_data)
+    qgr.close_browser()
+    # browser.close()
+    # browser.quit()
     print("connect to mysql")
     rm = register_mysql.RegisterMySQL()
     rm.register_tag_ranking(ranking_data)
